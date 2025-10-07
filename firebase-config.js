14d5c7882edbf863cc7e4608f5ce4ad5dd00f1c2
GAHO754 <atencionaclientes@gahospitality.com>
2025-10-07 17:11:56 -0600

    Initial Commit

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWta7DENSCSb-DN8OcKGa6uAbTbUYJhVg",
+  authDomain: "applebesapp.firebaseapp.com",
+  projectId: "applebesapp",
+  storageBucket: "applebesapp.firebasestorage.app",
+  messagingSenderId: "83305456668",
+  appId: "1:83305456668:web:3dfcfdd4eb2b6cb8982bd9",
+  measurementId: "G-WNTW6P44P3"
+};
+
+firebase.initializeApp(firebaseConfig);
