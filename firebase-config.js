6f1e2ba325c6eb389965e973e5f3ecc64c3751c0
GAHO754 <atencionaclientes@gahospitality.com>
2025-10-28 15:52:08 -0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0+ measurementId es opcional
 const firebaseConfig = {
   apiKey: "AIzaSyAWta7DENSCSb-DN8OcKGa6uAbTbUYJhVg",
   authDomain: "applebesapp.firebaseapp.com",
@@ -7,7 +6,8 @@ const firebaseConfig = {
   storageBucket: "applebesapp.firebasestorage.app",
   messagingSenderId: "83305456668",
   appId: "1:83305456668:web:3dfcfdd4eb2b6cb8982bd9",
-  measurementId: "G-WNTW6P44P3"
+  measurementId: "G-WNTW6P44P3",
+  databaseURL: "https://applebesapp-default-rtdb.firebaseio.com"  // ← FALTA
 };
 
 firebase.initializeApp(firebaseConfig);
