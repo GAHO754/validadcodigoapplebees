1d4d284768f8a978845ffa6b4f109c87e8356eea
GAHO754 <atencionaclientes@gahospitality.com>
2025-11-10 16:22:28 -0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,20 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAWta7DENSCSb-DN8OcKGa6uAbTbUYJhVg",
   authDomain: "applebesapp.firebaseapp.com",
-  databaseURL: "https://applebesapp-default-rtdb.firebaseio.com",
   projectId: "applebesapp",
-  storageBucket: "applebesapp.firebasestorage.app",
+
+  // ✅ URL correcto de tu base de datos Realtime
+  databaseURL: "https://applebesapp-default-rtdb.firebaseio.com",
+
+  // ⚠️ Corrige también el bucket (usa .appspot.com en lugar de .firebasestorage.app)
+  storageBucket: "applebesapp.appspot.com",
+
   messagingSenderId: "83305456668",
   appId: "1:83305456668:web:3dfcfdd4eb2b6cb8982bd9",
   measurementId: "G-WNTW6P44P3"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Inicializa Firebase solo si aún no está iniciado
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
